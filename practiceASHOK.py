ced97e0b97331c752f021ad7c90971905902f475
Sam Lamb <samlamb@Sams-MacBook-Pro.local>
2020-03-03 14:52:41 -0800

    added names, assignment date

--- practiceASHOK.py
+++ practiceASHOK.py
@@ -1,3 +1,8 @@
+#Partner 1: Ashok "Dammit Sam" "Not Fucking There, FUCK" Khare
+#Partner 2: Sam Isaac Lamb
+#########################################
+GitHub Practice for Wednesday 3/3/2020
+
 def getNRandom(n):
 	'''takes in an integer and returns a list of n random integers between 1 and 10, inclusive'''
     pass
